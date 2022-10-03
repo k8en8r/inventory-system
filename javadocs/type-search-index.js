6e8598c9f4db28d4e42e73e1616d17ff50f3e2d2
Caitie <114616263+k8en8r@users.noreply.github.com>
2022-10-03 16:12:16 -0700

    Add files via upload

---
+++ javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"controller","l":"FirstScreen"},{"p":"model","l":"InHouse"},{"p":"model","l":"Inventory"},{"p":"main","l":"main"},{"p":"model","l":"Outsourced"},{"p":"model","l":"Part"},{"p":"controller","l":"PartForm"},{"p":"model","l":"Product"},{"p":"controller","l":"ProductForm"}]
